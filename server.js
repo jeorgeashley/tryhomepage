91912fd2895ab43d58c16225b91006c353bfff47
jeorgeashley <mjabugaay@tip.edu.ph>
2025-03-14 12:12:19 +0800

    Add dynamic blog page

--- server.js
+++ server.js
@@ -1,23 +1,27 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the "public" folder
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files (CSS, JS, Images)
+app.use(express.static("public"));
 
-// Define routes for different pages
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+// API route to fetch blog posts
+app.get("/api/posts", (req, res) => {
+    fs.readFile(path.join(__dirname, "data", "posts.json"), "utf8", (err, data) => {
+        if (err) {
+            res.status(500).json({ error: "Error reading posts.json" });
+        } else {
+            res.json(JSON.parse(data));
+        }
+    });
 });
 
-app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'about.html'));
-});
-
-app.get('/contact', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'contact.html'));
+// Serve the blog page
+app.get("/blog", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "blog.html"));
 });
 
 // Start the server
